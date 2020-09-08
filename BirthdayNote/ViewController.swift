1b4230d54e5879b19e299de7a1665c55887e1469
letJamie <stillbut@naver.com>
2020-09-09 01:23:37 +0900

    Create UI and save data with User default

--- BirthdayNote/ViewController.swift
+++ BirthdayNote/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var birthTextField: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text!, forKey: "Name")
+        UserDefaults.standard.set(birthTextField.text!, forKey: "Birthday")
+        
+        fetchData()
+    }
+    
+    func fetchData() {
+        
+        nameLabel.text = UserDefaults.standard.string(forKey: "Name")
+        birthLabel.text = UserDefaults.standard.string(forKey: "Birthday")
+    }
 }
 
